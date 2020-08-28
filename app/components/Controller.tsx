2db3cc6de211743464833159c1d6a6d545597946
Promit Rai <promitrai@Promits-MacBook-Pro.local>
2020-08-28 11:17:24 +0100

    video integrated

---
+++ app/components/Controller.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {View, Slider, StyleSheet, Dimensions, Text} from 'react-native';
+import {Icon} from 'react-native-elements';
+const formatTime = (time: number) => {
+  //   let minutes = 0;
+  //   const seconds = Math.round(timeToFormat);
+  //   if (seconds > 60) {
+  //     minutes = Math.floor(seconds / 60);
+  //   }
+  //   let remainder = seconds % 60;
+  //   if (remainder < 10) {
+  //     if (remainder < 0) {
+  //       remainder = '00';
+  //     } else {
+  //       remainder = `0${remainder}`;
+  //     }
+  //   }
+  //   const time = `${minutes} : ${remainder}`;
+  //   return time;
+  const minutes = time >= 60 ? Math.floor(time / 60) : 0;
+  const seconds = Math.floor(time - minutes * 60);
+
+  return `${minutes >= 10 ? minutes : '0' + minutes}:${
+    seconds >= 10 ? seconds : '0' + seconds
+  }`;
+};
+export default Controller = ({
+  totalLength,
+  onDragSeekBar,
+  seekValue,
+  onSliderReleased,
+  onPlay,
+  paused,
+  fullScreen,
+  toggleFullScreen,
+}) => {
+  return (
+    <View style={styles.mainContainer}>
+      <Slider
+        step={1}
+        minimumValue={0}
+        maximumValue={totalLength}
+        value={seekValue}
+        minimumTrackTintColor="#0793DB"
+        maximumTrackTintColor="#FBFEFE"
+        onValueChange={(changedValue) => onDragSeekBar(changedValue)}
+        onSlidingComplete={(changedValue) => onSliderReleased(changedValue)}
+        style={{width: '100%', marginTop: 10}}
+      />
+      <View style={[styles.controllerContainer, {paddingHorizontal: 20}]}>
+        <View style={styles.controllerContainer}>
+          <Icon
+            name={paused ? 'play-sharp' : 'pause'}
+            type="ionicon"
+            color="black"
+            size={60}
+            onPress={onPlay}
+          />
+          <Text style={styles.textStyle}>{`${formatTime(
+            seekValue,
+          )} / ${formatTime(totalLength)}`}</Text>
+        </View>
+        <View style={styles.controllerContainer}>
+          <Icon name="volume-medium" type="ionicon" color="black" size={60} />
+          <Icon
+            name={fullScreen ? 'fullscreen-exit' : 'fullscreen'}
+            type="material-community"
+            color="black"
+            size={60}
+            onPress={toggleFullScreen}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    width: Dimensions.get('window').width / 2,
+  },
+  controllerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textStyle: {
+    marginLeft: 20,
+    fontSize: 20,
+    color: 'black',
+  },
+});
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,42 +1,132 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import Video from 'react-native-video';
+import Controller from '../components/Controller';
 import {Button} from 'react-native-elements';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 type FormData = {
   userId: string;
   password: string;
 };
 
-export default function Home({navigation}) {
-  return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        buttonStyle={styles.buttonStyle}
-        title="LOGIN"
-        onPress={() => navigation.navigate('Login')}
-        titleStyle={styles.buttonStyle}
-      />
-    </View>
-  );
+class Home extends React.Component {
+  state = {
+    paused: true,
+    loading: true,
+    fullScreen: false,
+    totalLength: 0,
+    currentTime: 0,
+    videoWidth: width / 2,
+    videoHeight: height / 2,
+  };
+  onPlay = () => {
+    this.setState({paused: !this.state.paused});
+  };
+  onLoad = (data) => {
+    this.setState({loading: false, totalLength: data.duration});
+  };
+  onEnd = () => {
+    this.setState({paused: true, currentTime: 0});
+  };
+  onProgress = (data) => {
+    this.setState({currentTime: data.currentTime});
+  };
+  onDrageSeekBar = () => {
+    this.setState({paused: true});
+  };
+  onSliderReleased = (currentTime) => {
+    this.setState({paused: false, currentTime});
+    this.player.seek(currentTime);
+  };
+
+  onToggleFullScreen = () => {
+    const {fullScreen} = this.state;
+    this.setState({
+      videoWidth: fullScreen ? width / 2 : width,
+      videoHeight: fullScreen ? height / 2 : height,
+    });
+  };
+
+  render() {
+    const {
+      totalLength,
+      currentTime,
+      paused,
+      fullScreen,
+      videoWidth,
+      videoHeight,
+    } = this.state;
+    return (
+      <View style={styles.mainContainer}>
+        <View style={[styles.videoContainer, {width: videoWidth}]}>
+          <Video
+            source={{
+              uri:
+                'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4',
+            }} // Can be a URL or a local file.
+            ref={(ref) => {
+              this.player = ref;
+            }}
+            style={{width: videoWidth, height: videoHeight}}
+            paused={paused}
+            controls={true}
+            progressUpdateInterval={50.0}
+            playInBackground
+            ignoreSilentSwitch="ignore"
+            playWhenInactive
+            onLoad={this.onLoad}
+            onEnd={this.onEnd}
+            onProgress={this.onProgress}
+          />
+          {/* <Controller
+            totalLength={totalLength}
+            seekValue={currentTime && currentTime}
+            onSliderReleased={this.onSliderReleased}
+            onDragSeekBar={this.onDrageSeekBar}
+            onPlay={this.onPlay}
+            paused={paused}
+            fullScreen={fullScreen}
+            toggleFullScreen={this.onToggleFullScreen}
+          /> */}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'black',
   },
   buttonStyle: {
     marginTop: 10,
-    paddingHorizontal: 80,
-    paddingVertical: 20,
+    paddingHorizontal: 50,
     borderRadius: 10,
+    backgroundColor: 'red',
     borderWidth: 1,
     borderColor: 'grey',
   },
   textStyle: {
     fontSize: 20,
-    color: 'grey',
+    color: 'white',
+  },
+  videoContainer: {
+    backgroundColor: 'white',
+  },
+  backgroundVideo: {
+    height: height / 2,
+    width: width / 2,
   },
 });
+
+export default Home;
